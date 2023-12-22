16fab6a6052b4215b0839a5df692108ffee2e7d8
Simon <555147@student.saxion.nl>
2023-12-22 12:27:41 +0100

    POP

---
+++ Cronoguard_3D/Assets/Game/Scripts/Player/WeaponRotation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class WeaponRotation : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    
+    [SerializeField] private LayerMask groundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Aim();
+    }
+
+    private void Aim()
+    {
+        var (success, position) = GetMousePosition();
+
+        if (success)
+        {
+            var direction = position - transform.position;
+            direction.y = 0;
+
+            transform.forward = direction;
+        }
+    }
+
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            return (success: false, position: Vector3.zero);
+        }
+    }
+}
