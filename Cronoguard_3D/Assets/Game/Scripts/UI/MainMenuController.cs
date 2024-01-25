71c185515f0089c851b124ac2de9a38d80e6c6d3
Lokulicious <lokugamedev@gmail.com>
2024-01-25 22:55:20 +0100

    Added Delete button and change text if there is a save

--- Cronoguard_3D/Assets/Game/Scripts/UI/MainMenuController.cs
+++ Cronoguard_3D/Assets/Game/Scripts/UI/MainMenuController.cs
@@ -2,13 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] SavingGame savingManager;
-    
+    [SerializeField] private TextMeshProUGUI[] slotText;
+
+    private void Start()
+    {
+        SetSlotText();
+    }
+
     public void StartGame(string save)
     {
         savingManager.currentSave = save;
@@ -20,10 +27,25 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetSlotText()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (TryGetSave("slot" + (i+1)))
+            {
+                slotText[i].text = "Load save " + (i + 1);
+            }
+            else
+            {
+                slotText[i].text = "Start New Game";
+            }
+        }
+    }
+
 
     bool TryGetSave(string save)
     {
-        if (File.Exists(Application.persistentDataPath + "/" + name + ".save"))
+        if (File.Exists(Application.persistentDataPath + "/" + save + ".save"))
         {
             return true;
         }
